ebda86a73fd79881c04b9b2272153a8779e2e2c8
Imran <imran@zeropoint.it>
2018-03-19 18:15:08 +0500

    Greet new user and alert others.

--- server/server.js
+++ server/server.js
@@ -31,11 +31,28 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {//emit the message to every single connection
+        socket.emit('newMessage', {
+            from: "Admin",
+            text: "Welcome to the chat!",
+            createdAt: new Date().getTime()
+        });
+
+        socket.broadcast.emit('newMessage', {
+            from: "Admin",
+            text: "New user joined.",
+            createdAt: new Date().getTime()
+        });
+        /*io.emit('newMessage', {//emit the message to every single connection
             from:message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        });
+        });*/
+
+        /*socket.broadcast.emit('newMessage', {//broadcast message to all but not this user
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });*/
     });
 
     /*socket.emit('newMessage', {
