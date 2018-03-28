c9aa0d2119adc7449109618668634867b63decbe
Imran <imran@zeropoint.it>
2018-03-28 19:27:10 +0500

    Added mustache.js for templates.

--- public/js/index.js
+++ public/js/index.js
@@ -23,22 +23,26 @@ socket.on('newEmail', function(data) {
 
 socket.on('newMessage', function(message) {
     console.log('newMessage', message);
+    var template = jQuery('#message-template').html();
     var formattedTime = moment(moment.createdAt).format('h:mm a');
-
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-    jQuery('#messages').append(li);
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
-socket.on('newLocationMessage', function (message) {
+socket.on('newLocationMessage', function (message) {   
+    var template = jQuery('#location-message-template').html();
     var formattedTime = moment(moment.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime,
+        url: message.url
+    });
+    jQuery('#messages').append(html);
 });
 
 /*socket.emit('createMessage', {
