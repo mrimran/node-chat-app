524a37368486d751f0b727a296f970777e86fff0
Imran <imran@zeropoint.it>
2018-03-28 19:45:16 +0500

    Scroll to bottom if user is closer to bottom.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom () {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connected to server');
     /*socket.emit('createEmail', {
@@ -31,6 +48,7 @@ socket.on('newMessage', function(message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {   
@@ -43,6 +61,7 @@ socket.on('newLocationMessage', function (message) {
         url: message.url
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 /*socket.emit('createMessage', {
