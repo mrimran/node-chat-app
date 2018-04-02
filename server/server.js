45d817b61d842f56c7785ea2fea5e5ab948eaa00
Imran <imran@zeropoint.it>
2018-04-02 15:15:30 +0500

    Seding messages to private rooms.

--- server/server.js
+++ server/server.js
@@ -41,7 +41,11 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+        var user = users.getUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+        }
     });
 
     socket.on('join', (params, callback) => {
@@ -66,8 +70,12 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        var user = users.getUser(socket.id);
+
+        if(user && isRealString(message.text)) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
+
         callback("This is from the server.");//ack
         /*io.emit('newMessage', {//emit the message to every single connection
             from:message.from,
